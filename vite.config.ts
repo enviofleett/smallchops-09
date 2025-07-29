67d3cc1c36fdc3866028f072134ec22ad7c654f9
enviofleett <enviofleett@gmail.com>
2025-07-29 17:46:10 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig(({ mode }) => ({
   build: {
     outDir: "dist",
     sourcemap: mode !== "production",
-    minify: mode === "production" ? "terser" : false,
+    minify: false,
     rollupOptions: {
       output: {
         manualChunks: {
